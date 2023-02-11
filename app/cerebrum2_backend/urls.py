0f6e92a5c7d2d4ad0cdf512bfacff1c9525efc9e
Alex Kao <alexkxosb@gmail.com>
2023-02-11 18:59:38 -0500

    Static files

--- app/cerebrum2_backend/urls.py
+++ app/cerebrum2_backend/urls.py
@@ -2,6 +2,8 @@ from django.urls import include, path
 from django.contrib import admin
 from rest_framework import routers
 from api import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register(r"users", views.UserViewSet)
@@ -13,4 +15,4 @@ urlpatterns = [
     path("", include(router.urls)),
     path("admin", admin.site.urls),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
